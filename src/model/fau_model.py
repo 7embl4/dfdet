5db2049048741f2010f4dcb2e650ba2f0ce418a5
7embl4 <art3mka1408@gmail.com>
2026-04-22 16:53:19 +0300

    Finish FAU expert

--- src/model/fau_model.py
+++ src/model/fau_model.py
@@ -6,10 +6,14 @@ from src.model.fau_detector import FAUDetector
 
 
 class FAUModel(nn.Module):
+    """
+    Facial Action Units (FAUs) Expert based on HAUTE (https://dl.acm.org/doi/10.1145/3774904.3792919).
+    TL;DR: FAUs analysed with BiLSTM and image embeddings from CLIP are fused in multihead attention.
+    """
     def __init__(
             self, 
             # fau lstm
-            input_features=7,
+            input_features=12,
             hidden_size=64,
             num_layers=2,
             bidirectional=True,
@@ -22,20 +26,21 @@ class FAUModel(nn.Module):
             fusion_dropout=0.1,
 
             # classification head
-            reduction=2,
             cls_dropout=0.3,
             num_classes=2,
 
             # other
-            device="cpu", 
             *args, 
             **kwargs
         ):
-        super().__init__(*args, **kwargs)
-        self.device = device
+        """
+        Args:
 
+        """
+        super().__init__(*args, **kwargs)
         # fau branch
-        self.fau_extractor = FAUDetector()
+        self.fau_detector = FAUDetector()
+        self.fau_detector.eval()
         self.fau_lstm = nn.LSTM(
             input_size=input_features, 
             hidden_size=hidden_size,
@@ -43,13 +48,15 @@ class FAUModel(nn.Module):
             bidirectional=bidirectional,
             batch_first=True,
         )
+        d_model = 2 * hidden_size if bidirectional else hidden_size
 
         # image branch
-        self.image_encoder, _ = clip.load(encoder_model, device=self.device)
-        self.image_proj = nn.Linear()
+        self.image_encoder, _ = clip.load(encoder_model)
+        self.image_encoder.eval()
+        clip_emb_dim = self.image_encoder.token_embedding.embedding_dim
+        self.enc_proj = nn.Linear(clip_emb_dim, d_model)
 
         # fusion
-        d_model = 2 * hidden_size if bidirectional else hidden_size
         self.attention = nn.MultiheadAttention(
             embed_dim=d_model,
             num_heads=num_heads,
@@ -59,10 +66,42 @@ class FAUModel(nn.Module):
 
         # classifier
         self.classifier = nn.Sequential(
-            nn.Linear(d_model, d_model // reduction),
-            nn.ReLU(),
-            nn.Linear(d_model // reduction, num_classes)
+            nn.Linear(d_model, d_model // 2),
+            nn.BatchNorm1d(d_model // 2),
+            nn.PReLU(),
+            nn.Dropout(cls_dropout),
+            nn.Linear(d_model // 2, d_model // 4),
+            nn.BatchNorm1d(d_model // 4),
+            nn.PReLU(),
+            nn.Dropout(cls_dropout),
+            nn.Linear(d_model // 4, num_classes),
         )
 
     def forward(self, frames: torch.Tensor, **batch):
-        pass
+        """
+        Args:
+            frames (torch.Tensor): batch of videos with size [B, T, C, H, W]
+        """
+        # get aus and embeddings
+        with torch.no_grad():
+            aus = torch.stack([self.fau_detector(video) for video in frames])   # [B, T, D]
+            embeddings = torch.stack(
+                [self.image_encoder.encode_image(video) for video in frames]
+            ).float() # [B, T, D_enc]
+
+        # lstm and projection
+        aus, (h_n, c_n) = self.fau_lstm(aus)  # [B, T, 2*D]
+        print(embeddings.dtype)
+        print(self.enc_proj.weight.dtype)
+        embeddings = self.enc_proj(embeddings)  # [B, T, 2*D]
+
+        # attention fusion and classification
+        out, _ = self.attention(
+            query=aus, 
+            key=embeddings, 
+            value=embeddings
+        )  # [B, T, 2*D]
+        out = torch.mean(out, dim=1)  # [B, 2*D]
+        out = self.classifier(out)
+
+        return out
