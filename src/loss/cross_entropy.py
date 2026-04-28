3418624d71cb3e63261c048158bf36baf7307b2a
7embl4 <art3mka1408@gmail.com>
2026-04-28 13:56:18 +0300

    Add label smoothing

--- src/loss/cross_entropy.py
+++ src/loss/cross_entropy.py
@@ -8,14 +8,14 @@ from src.loss import BaseLoss
 
 
 class CrossEntropyLoss(BaseLoss):
-    def __init__(self, train_dataset, *args, **kwargs):
+    def __init__(self, train_dataset, label_smoothing, *args, **kwargs):
         super().__init__(*args, **kwargs)
         device = "cuda" if torch.cuda.is_available() else "cpu"
         
         y = [item["target"] for item in train_dataset._index]
         class_weights = compute_class_weight(class_weight="balanced", classes=np.unique(y), y=y)
         class_weights = torch.tensor(class_weights, dtype=torch.float).to(device)
-        self.criterion = nn.CrossEntropyLoss(weight=class_weights)
+        self.criterion = nn.CrossEntropyLoss(weight=class_weights, label_smoothing=label_smoothing)
     
     def update(self, pred: torch.Tensor, target: torch.Tensor, **batch):
         return self.criterion(pred, target)
